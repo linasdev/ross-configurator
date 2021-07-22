780f7930c6cb3813f44fce2a67a28350db750feb
Linas Nikiperavičius <linas@linasdev.com>
2021-07-22 20:21:09 +0300

    Change default baudrate to 115200

--- src/ross_configurator.rs
+++ src/ross_configurator.rs
@@ -3,7 +3,7 @@ use std::io::Error as IOError;
 use crate::ross_serial::RossSerialError;
 
 pub const PACKET_TIMEOUT_MS: u128 = 500;
-pub const DEFAULT_BAUDRATE: u32 = 9_600;
+pub const DEFAULT_BAUDRATE: u32 = 115_200;
 pub const DATA_PACKET_SIZE: usize = 128;
 
 #[derive(Debug)]
