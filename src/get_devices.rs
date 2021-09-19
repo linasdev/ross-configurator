ee20c782025233f85f3b36e05c83c0dae2682b15
Linas Nikiperavičius <linas@linasdev.com>
2021-09-19 15:39:36 +0300

    Deduplicate devices after getting bootloader hello events

--- src/get_devices.rs
+++ src/get_devices.rs
@@ -12,12 +12,14 @@ use crate::get_programmer::get_programmer;
 pub fn get_devices(protocol: &mut Protocol<Serial>) -> Result<Vec<BootloaderHelloEvent>, ConfiguratorError>  {
     let programmer_hello_event = get_programmer(protocol)?;
 
-    let devices: Vec<BootloaderHelloEvent> = match protocol.exchange_packets(programmer_hello_event.to_packet(), false, TRANSACTION_RETRY_COUNT as u32, || {
+    let mut devices: Vec<BootloaderHelloEvent> = match protocol.exchange_packets(programmer_hello_event.to_packet(), false, TRANSACTION_RETRY_COUNT as u32, || {
         sleep(Duration::from_millis(PACKET_TIMEOUT_MS))
     }) {
         Ok(event) => event,
         Err(err) => return Err(ConfiguratorError::ProtocolError(err)),
     };
+
+    devices.dedup();
 
     for bootloader_hello_event in devices.iter() {
         println!("Found device (address: {:#06x}, firmware_version: {:#010x})", bootloader_hello_event.bootloader_address, bootloader_hello_event.firmware_version);
